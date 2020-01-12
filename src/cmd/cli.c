9c66c721d0bc1905f2cfc644fa56e1e0d701fc77
Jonathan McDowell <noodles@earth.li>
2020-01-12 19:50:35 +0000

    Add CLI mode skeleton code
    Put in the beginnings of support for multiple CLI protocol modes. Just
    HiZ for now, and a general state structure for the CLI.

--- src/cmd/cli.c
+++ src/cmd/cli.c
@@ -18,86 +18,101 @@
 #include "gpio.h"
 #include "tty.h"
 
-static void cli_aux_read(struct cdc *tty)
+enum cli_mode {
+	MODE_HIZ = 0,
+	MODE_MAX
+};
+
+const char *cli_mode_str[] = {
+	"HiZ",
+};
+
+struct cli_state {
+	struct cdc *tty;
+	enum cli_mode mode;
+};
+
+static void cli_aux_read(struct cli_state *state)
 {
 	/* Set AUX to input */
 	gpio_set_direction(PIN_AUX, true);
-	tty_printf(tty, "AUX INPUT/HI-Z, READ: ");
-	tty_putc(tty, gpio_get(PIN_AUX) ? '1' : '0');
-	tty_printf(tty, "\r\n");
+	tty_printf(state->tty, "AUX INPUT/HI-Z, READ: ");
+	tty_putc(state->tty, gpio_get(PIN_AUX) ? '1' : '0');
+	tty_printf(state->tty, "\r\n");
 }
 
-static void cli_aux_set(struct cdc *tty, bool on)
+static void cli_aux_set(struct cli_state *state, bool on)
 {
 	/* Set AUX to output */
 	gpio_set_direction(PIN_AUX, false);
 	gpio_set(PIN_AUX, on);
-	tty_printf(tty, on ? "AUX HIGH\r\n" : "AUX LOW\r\n");
+	tty_printf(state->tty, on ? "AUX HIGH\r\n" : "AUX LOW\r\n");
 }
 
-static void cli_banner(struct cdc *tty)
+static void cli_banner(struct cli_state *state)
 {
-	tty_printf(tty, "DeskViking v0.1\r\n");
-	tty_printf(tty, "Board name: " BOARD_NAME ", SYS Version: ");
-	tty_putc(tty, sys_version[2]);
-	tty_putc(tty, sys_version[4]);
-	tty_putc(tty, sys_version[6]);
-	tty_printf(tty, "\r\n");
+	tty_printf(state->tty, "DeskViking v0.1\r\n");
+	tty_printf(state->tty, "Board name: " BOARD_NAME ", SYS Version: ");
+	tty_putc(state->tty, sys_version[2]);
+	tty_putc(state->tty, sys_version[4]);
+	tty_putc(state->tty, sys_version[6]);
+	tty_printf(state->tty, "\r\n");
 }
 
-static void cli_delay(struct cdc *tty, bool ms)
+static void cli_delay(struct cli_state *state, bool ms)
 {
 	if (ms) {
-		tty_printf(tty, "DELAY 1ms\r\n");
+		tty_printf(state->tty, "DELAY 1ms\r\n");
 		chopstx_usec_wait(1000);
 	} else {
-		tty_printf(tty, "DELAY 1µs\r\n");
+		tty_printf(state->tty, "DELAY 1µs\r\n");
 		chopstx_usec_wait(1);
 	}
 }
 
-static void cli_help(struct cdc *tty)
+static void cli_help(struct cli_state *state)
 {
-	tty_printf(tty, "General\r\n");
-	tty_printf(tty, "--------------------------------\r\n");
-	tty_printf(tty, "?      Help\r\n");
-	tty_printf(tty, "#      Reset CLI state\r\n");
-	tty_printf(tty, "&/%    Delay 1µs/ms\r\n");
-	tty_printf(tty, "a/A/@  Set AUX low/HI/read value\r\n");
-	tty_printf(tty, "i      Version/status info\r\n");
-	tty_printf(tty, "v      Show volts/states\r\n");
+	tty_printf(state->tty, "General\r\n");
+	tty_printf(state->tty, "--------------------------------\r\n");
+	tty_printf(state->tty, "?      Help\r\n");
+	tty_printf(state->tty, "#      Reset CLI state\r\n");
+	tty_printf(state->tty, "&/%    Delay 1µs/ms\r\n");
+	tty_printf(state->tty, "a/A/@  Set AUX low/HI/read value\r\n");
+	tty_printf(state->tty, "i      Version/status info\r\n");
+	tty_printf(state->tty, "v      Show volts/states\r\n");
 }
 
-static void cli_reset(struct cdc *tty)
+static void cli_reset(struct cli_state *state)
 {
-	/* No-op for now, will reset back to HiZ eventually */
-	tty_printf(tty, "RESET\r\n\r\n");
-	cli_banner(tty);
+	tty_printf(state->tty, "RESET\r\n\r\n");
+	state->mode = MODE_HIZ;
+	/* TODO: Reset back to all inputs */
+	cli_banner(state);
 }
 
-static void cli_states(struct cdc *tty)
+static void cli_states(struct cli_state *state)
 {
-	tty_printf(tty, "Pinstates:\r\n");
-	tty_printf(tty, "GND\t3.3V\t5.0V\tADC\tVPU\tAUX\tCLK\tMOSI\tCS\tMISO\r\n");
+	tty_printf(state->tty, "Pinstates:\r\n");
+	tty_printf(state->tty, "GND\t3.3V\t5.0V\tADC\tVPU\tAUX\tCLK\tMOSI\tCS\tMISO\r\n");
 
-	tty_printf(tty, "P\tP\tP\tI\tI\t");
-	tty_printf(tty, gpio_get_direction(PIN_AUX) ? "I\t" : "O\t");
-	tty_printf(tty, gpio_get_direction(PIN_CLK) ? "I\t" : "O\t");
-	tty_printf(tty, gpio_get_direction(PIN_MOSI) ? "I\t" : "O\t");
-	tty_printf(tty, gpio_get_direction(PIN_CS) ? "I\t" : "O\t");
-	tty_printf(tty, gpio_get_direction(PIN_MISO) ? "I\t" : "O\t");
-	tty_printf(tty, "\r\n");
+	tty_printf(state->tty, "P\tP\tP\tI\tI\t");
+	tty_printf(state->tty, gpio_get_direction(PIN_AUX) ? "I\t" : "O\t");
+	tty_printf(state->tty, gpio_get_direction(PIN_CLK) ? "I\t" : "O\t");
+	tty_printf(state->tty, gpio_get_direction(PIN_MOSI) ? "I\t" : "O\t");
+	tty_printf(state->tty, gpio_get_direction(PIN_CS) ? "I\t" : "O\t");
+	tty_printf(state->tty, gpio_get_direction(PIN_MISO) ? "I\t" : "O\t");
+	tty_printf(state->tty, "\r\n");
 
-	tty_printf(tty, "GND\t3.3V\t5.0V\t?\t?\t");
-	tty_printf(tty, gpio_get(PIN_AUX) ? "H\t" : "L\t");
-	tty_printf(tty, gpio_get(PIN_CLK) ? "H\t" : "L\t");
-	tty_printf(tty, gpio_get(PIN_MOSI) ? "H\t" : "L\t");
-	tty_printf(tty, gpio_get(PIN_CS) ? "H\t" : "L\t");
-	tty_printf(tty, gpio_get(PIN_MISO) ? "H\t" : "L\t");
-	tty_printf(tty, "\r\n");
+	tty_printf(state->tty, "GND\t3.3V\t5.0V\t?\t?\t");
+	tty_printf(state->tty, gpio_get(PIN_AUX) ? "H\t" : "L\t");
+	tty_printf(state->tty, gpio_get(PIN_CLK) ? "H\t" : "L\t");
+	tty_printf(state->tty, gpio_get(PIN_MOSI) ? "H\t" : "L\t");
+	tty_printf(state->tty, gpio_get(PIN_CS) ? "H\t" : "L\t");
+	tty_printf(state->tty, gpio_get(PIN_MISO) ? "H\t" : "L\t");
+	tty_printf(state->tty, "\r\n");
 }
 
-static void cli_process_cmd(struct cdc *tty, const char *cmd, unsigned int len)
+static void cli_process_cmd(struct cli_state *state, const char *cmd, unsigned int len)
 {
 	while (len > 0) {
 		switch (*(cmd++)) {
@@ -105,34 +120,34 @@ static void cli_process_cmd(struct cdc *tty, const char *cmd, unsigned int len)
 			/* Ignore */
 			break;
 		case '?':
-			cli_help(tty);
+			cli_help(state);
 			break;
 		case '#':
-			cli_reset(tty);
+			cli_reset(state);
 			break;
 		case '&':
-			cli_delay(tty, false);
+			cli_delay(state, false);
 			break;
 		case '%':
-			cli_delay(tty, true);
+			cli_delay(state, true);
 			break;
 		case '@':
-			cli_aux_read(tty);
+			cli_aux_read(state);
 			break;
 		case 'a':
-			cli_aux_set(tty, false);
+			cli_aux_set(state, false);
 			break;
 		case 'A':
-			cli_aux_set(tty, true);
+			cli_aux_set(state, true);
 			break;
 		case 'i':
-			cli_banner(tty);
+			cli_banner(state);
 			break;
 		case 'v':
-			cli_states(tty);
+			cli_states(state);
 			break;
 		default:
-			tty_printf(tty, "Unknown command.\r\n");
+			tty_printf(state->tty, "Unknown command.\r\n");
 		}
 		len--;
 	}
@@ -140,25 +155,30 @@ static void cli_process_cmd(struct cdc *tty, const char *cmd, unsigned int len)
 
 void cli_main(struct cdc *tty, const char *s, int len)
 {
+	struct cli_state state;
 	char cmd[65];
-	unsigned int cmd_len;
+	int cmd_len;
 
 	(void)s;
 	(void)len;
 
-	cli_banner(tty);
+	state.tty = tty;
+	state.mode = MODE_HIZ;
+
+	cli_banner(&state);
 
 	while (1) {
+		tty_printf(tty, cli_mode_str[state.mode]);
 		tty_printf(tty, ">");
 		cmd_len = tty_readline(tty, cmd, sizeof(cmd));
 		/* Process cmd */
 		if (cmd_len > 0) {
 			cmd[cmd_len] = 0;
-			cli_process_cmd(tty, cmd, cmd_len);
+			cli_process_cmd(&state, cmd, cmd_len);
 			cmd_len = 0;
-		} else if (cmd_len < 0) {
-			break;
 		}
+		if (cmd_len < 0)
+			break;
 	}
 
 	debug_print("Leaving interactive mode.\r\n");
